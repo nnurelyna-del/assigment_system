6ba11622755217a84d6530be6e9d50952169ddae
Elyna Natasha <nnurelyna@gmail.com>
2026-04-27 19:51:44 +0800

    edit code mispelled

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "assignment_system");
+$conn = new mysqli("localhost", "root", "", "assigment_system");
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 ?>
